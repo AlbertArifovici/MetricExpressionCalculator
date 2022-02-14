fb4f16b462baee9b2e193fc57ba9547d3a98538b
AlbertArifovici <97481021+AlbertArifovici@users.noreply.github.com>
2022-02-14 19:42:28 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-package testing;
-
-public class Main {
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.calculate("10 cm + 1 m - 10 mm"));
-    }
-}
-
