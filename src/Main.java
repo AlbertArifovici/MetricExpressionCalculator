d56b17b836c4f242efa22c93d88b950f6840c2ca
AlbertArifovici <97481021+AlbertArifovici@users.noreply.github.com>
2022-02-14 19:41:11 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package testing;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.calculate("10 cm + 1 m - 10 mm"));
+    }
+}
+
